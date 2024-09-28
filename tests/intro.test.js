3406b4503b3a59d6c4a016ec0e5834ba7bdd9b60
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-28 13:23:02 +0530

    refactor: updated test name in max - 3rd testcase

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -8,7 +8,7 @@ describe("max", () => {
     it("should return the second argument if it is greater", () => {
         expect(max(1, 2)).toBe(2);
     })
-    it("should return the first arguments are equal", () => {
+    it("should return the first argument if arguments are equal", () => {
         expect(max(1, 1)).toBe(1);
     })
 })
