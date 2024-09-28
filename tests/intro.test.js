e68a6c78c1e1558bdb160f40e460e19db839aa48
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-28 13:19:23 +0530

    refactor: re structured the tests from AAA in to single line

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -3,32 +3,12 @@ import { max } from "../src/intro";
 
 describe("max", () => {
     it("should return the first argument if it is greater", () => {
-        // AAA: A structured approach for writing tests
-
-        // Arrange:  Set up the necessary preconditions and inputs for the test
-        const a = 2;
-        const b = 1;
-
-        // Act: Execute the functionality being tested
-        const result = max(a, b);
-
-        // Assert: Validate the outcome against expected results
-        expect(result).toBe(a);
+        expect(max(2, 1)).toBe(2);
     })
     it("should return the second argument if it is greater", () => {
-        const a = 1;
-        const b = 2;
-
-        const result = max(a, b);
-
-        expect(result).toBe(b);
+        expect(max(1, 2)).toBe(2);
     })
     it("should return the first argument if it is equal", () => {
-        const a = 1;
-        const b = 1;
-
-        const result = max(a, b);
-
-        expect(result).toBe(a);
+        expect(max(1, 1)).toBe(1);
     })
 })
