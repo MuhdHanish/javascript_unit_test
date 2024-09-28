9c604a63c6d69ce401345aecc2fbd62b11d9190e
MuhdHanish <muhammedhanish11@gmail.com>
2024-09-28 14:02:49 +0530

    feat: added test for fizzBuzz function

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
     it("should return the first argument if it is greater", () => {
@@ -12,3 +12,18 @@ describe("max", () => {
         expect(max(1, 1)).toBe(1);
     })
 })
+
+describe("fizzBuzz", () => {
+    it("should return 'FizzBuzz' if the argument is a multiple of both 3 and 5", () => {
+        expect(fizzBuzz(15)).toBe("FizzBuzz");
+    })
+    it("should retrun 'Fizz' if the argument a multiple of 3", () => {
+        expect(fizzBuzz(9)).toBe("Fizz");
+    })
+    it("should return 'Buzz' if the argument a multiple of 5", () => {
+        expect(fizzBuzz(10)).toBe("Buzz");
+    })
+    it("should return the argument as string if the argument is not a multiple of either 3 or 5", () => {
+        expect(fizzBuzz(8)).toBe('8');
+    })
+}) 
